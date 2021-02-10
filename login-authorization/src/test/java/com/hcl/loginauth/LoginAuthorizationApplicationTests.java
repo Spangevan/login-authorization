03a119b14361f5bd7f0eb6d015581dd75389bf5a
Spangevan <ewlson23@gmail.com>
2021-02-10 18:24:49 -0500

    added 3 tests to the application

--- login-authorization/src/test/java/com/hcl/loginauth/LoginAuthorizationApplicationTests.java
+++ login-authorization/src/test/java/com/hcl/loginauth/LoginAuthorizationApplicationTests.java
@@ -1,13 +1,42 @@
 package com.hcl.loginauth;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class LoginAuthorizationApplicationTests {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Autowired
+	private MvcConfig mvcConfig;
+	
+	
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(mvcConfig).isNotNull();
 	}
-
+	
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+				String.class)).contains("Welcome!");
+	}
+	
+	@Test
+	public void loginShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/login",
+				String.class)).contains("User Name");
+	}
 }
