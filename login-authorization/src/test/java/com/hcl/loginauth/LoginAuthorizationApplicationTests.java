931343ddbaa7086d6aacc4c817a27168ec5b75ee
Spangevan <ewlson23@gmail.com>
2021-02-10 18:55:31 -0500

    minor edits

--- login-authorization/src/test/java/com/hcl/loginauth/LoginAuthorizationApplicationTests.java
+++ login-authorization/src/test/java/com/hcl/loginauth/LoginAuthorizationApplicationTests.java
@@ -11,32 +11,30 @@ import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class LoginAuthorizationApplicationTests {
-	
+
 	@LocalServerPort
 	private int port;
-	
+
 	@Autowired
 	private TestRestTemplate restTemplate;
-	
+
 	@Autowired
 	private MvcConfig mvcConfig;
-	
-	
 
 	@Test
 	public void contextLoads() throws Exception {
 		assertThat(mvcConfig).isNotNull();
 	}
-	
+
 	@Test
 	public void greetingShouldReturnDefaultMessage() throws Exception {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-				String.class)).contains("Welcome!");
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", String.class)).contains("Welcome!");
 	}
-	
+
 	@Test
 	public void loginShouldReturnDefaultMessage() throws Exception {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/login",
-				String.class)).contains("User Name");
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/login", String.class))
+				.contains("User Name");
 	}
+
 }
