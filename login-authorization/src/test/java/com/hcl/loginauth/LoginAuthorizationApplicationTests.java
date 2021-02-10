db1a9e566de46dd62eb48ac274d166d9e567f330
Spangevan <ewlson23@gmail.com>
2021-02-10 17:18:32 -0500

    initial commit to repository

---
+++ login-authorization/src/test/java/com/hcl/loginauth/LoginAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.loginauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
